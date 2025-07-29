cf49be97ce5e1d516c60571415975073e761c4bd
restjun <150533379+restjun@users.noreply.github.com>
2025-07-29 14:19:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -200,7 +200,7 @@ def send_ranked_volume_message(bullish_ids):
         volume_24h_data[inst_id] = vol_24h
         time.sleep(random.uniform(0.2, 0.4))
 
-    top_3_ids = sorted(volume_24h_data.items(), key=lambda x: x[1], reverse=True)[:10]
+    top_3_ids = sorted(volume_24h_data.items(), key=lambda x: x[1], reverse=True)[:3]
     top_3_ids = [item[0] for item in top_3_ids]
 
     for inst_id in top_3_ids:
@@ -215,7 +215,7 @@ def send_ranked_volume_message(bullish_ids):
     ]
 
     message_lines = [
-        "📅 *[정배열 5/20/50] + [거래대금 Top10]*",
+        "📅 *[정배열 5/20/50] + [거래대금 Top3]*",
         "━━━━━━━━━━━━━━━━━━━",
         f"💰 *BTC* {btc_change_str} / 거래대금: {btc_volume_str}",
         f"    {btc_ema_status}",
