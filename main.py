1c7c04ccc9eb466841b64955c0aa1750df543677
restjun <150533379+restjun@users.noreply.github.com>
2025-08-23 19:01:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -166,7 +166,7 @@ def get_all_timeframe_ema_status(inst_id):
         return "❌ 상태 계산 실패"
 
 def calculate_1h_volume(inst_id):
-    df = get_ohlcv_okx(inst_id, bar="1H", limit=4)
+    df = get_ohlcv_okx(inst_id, bar="1H", limit=24)
     if df is None or len(df) < 1:
         return 0
     return df["volCcyQuote"].sum()
