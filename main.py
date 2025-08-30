c4c39bce5f4f36050b09e09e0ce477c07621db01
restjun <150533379+restjun@users.noreply.github.com>
2025-08-30 21:36:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -159,7 +159,7 @@ def get_24h_volume(inst_id):
 def send_new_entry_message(all_ids):
     global sent_signal_coins
     volume_map = {inst_id: get_24h_volume(inst_id) for inst_id in all_ids}
-    top_ids = sorted(volume_map, key=volume_map.get, reverse=True)[:50]
+    top_ids = sorted(volume_map, key=volume_map.get, reverse=True)[:15]
     rank_map = {inst_id: rank+1 for rank, inst_id in enumerate(top_ids)}
     new_entry_coins = []
 
@@ -175,7 +175,7 @@ def send_new_entry_message(all_ids):
             continue
 
         daily_change = calculate_daily_change(inst_id)
-        if daily_change is None or daily_change <= 5:
+        if daily_change is None or daily_change <= 0:
             continue
 
         if not sent_signal_coins[inst_id]["crossed"]:
