7fab36e7efec3775ba720233db714221c7ba9dd6
restjun <150533379+restjun@users.noreply.github.com>
2025-09-03 12:00:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -159,7 +159,7 @@ def get_all_okx_swap_symbols():
 # 24시간 거래대금
 # =========================
 def get_24h_volume(inst_id):
-    df = get_ohlcv_okx(inst_id, bar="1H", limit=24)
+    df = get_ohlcv_okx(inst_id, bar="1H", limit=4)
     if df is None or len(df) < 24:
         return 0
     return df['volCcyQuote'].sum()
