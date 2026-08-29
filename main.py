90bd5de43d306c3e8841b7c348efe3d649b9b025
restjun <150533379+restjun@users.noreply.github.com>
2026-08-29 18:15:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ logging.basicConfig(
 
 VOLUME_HOURS = 24
 
-TOP_N = 30
+TOP_N = 20
 
 UPDATE_MINUTES = 5
 
@@ -2316,7 +2316,7 @@ def direction_html(
 
         return (
             '<span class="direction-long">'
-            '☀️☀️☀️'
+            '✈️✈️✈️'
             '</span>'
         )
 
@@ -2330,7 +2330,7 @@ def direction_html(
 
         return (
             '<span class="direction-short">'
-            '🌧🌧🌧'
+            '🛩🛩🛩'
             '</span>'
         )
 
