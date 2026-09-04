3bbb1b019bc1c4f2771bc8543f72ed98c00e23bf
restjun <150533379+restjun@users.noreply.github.com>
2026-09-04 16:12:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2713,7 +2713,7 @@ def rising_focus_section(
     return f"""
     <h2 class="focus-title rising-title">
 
-        🚀 상승 경고 리스트 [1H(30) 이상 확인✨️]
+        🚀 상승 경고 리스트 [1H(10)이상/첫번째 〽️]
 
         <small>
             {update_time} KST
