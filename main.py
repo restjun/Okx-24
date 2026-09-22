5b777b06e6e1318ec330bc2411054ff55cf0bdbb
restjun <150533379+restjun@users.noreply.github.com>
2026-09-22 16:50:39 +0900

    Adjust display and flash count minimums and maximums

--- main.py
+++ main.py
@@ -84,16 +84,16 @@ ROC_FILTER_COUNT_MIN = 10
 # 화면 COUNT 기준
 # =========================================================
 
-DISPLAY_COUNT_MIN = 1
-DISPLAY_COUNT_MAX = 3
+DISPLAY_COUNT_MIN = 0
+DISPLAY_COUNT_MAX = 2
 
 
 # =========================================================
 # 반짝임 COUNT
 # =========================================================
 
-FLASH_COUNT_MIN = 1
-FLASH_COUNT_MAX = 1
+FLASH_COUNT_MIN = 0
+FLASH_COUNT_MAX = 2
 
 
 # =========================================================
